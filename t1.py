f89eb040369ee8f8d05ea865db17a496a4ce9f04
unknown <praveenkumarmath2@gmail.com>
2026-03-08 00:42:41 +0530

    Chekc sample

---
+++ t1.py
@@ -0,0 +1,25 @@
+# This script perfroms
+# addition of 2 numbers
+# where number1 is x
+# number2 is y
+
+
+
+x = 10 # this number 1
+y = 20 # this number 2
+
+out = x + y # This is addition of number 1 and number 2
+
+
+
+x, y = 2, 4 
+
+print("Initially")
+print("The value of x is ", x)
+print("The value of y is ", y)
+
+x, y = y, x+y
+
+print("After")
+print("The value of x is ", x)
+print("The value of y is ", y)
